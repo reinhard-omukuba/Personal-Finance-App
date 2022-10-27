9380b6455677753edd9b01959e1520334f9a2ac0
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-10-27 09:57:46 +0300

    pull all expense

--- src/Pages/Expenses.js
+++ src/Pages/Expenses.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LeftNav from '../Components/LeftNav'
 import NavBar from '../Components/NavBar'
 import { Button } from 'react-bootstrap'
 import {useNavigate} from "react-router-dom";
 
+//firebase
+import '../firebase';
+import {db} from '../firebase'
+import { collection, getDocs, QuerySnapshot } from "firebase/firestore";
+
 function Expenses() {
 
+  //pull all expenses
+  const [expenseList, setExpenseList] = useState([]);
+
+  useEffect(()=>{
+    const expenseData =  getDocs(collection(db, "expenses"));
+    expenseData.then((QuerySnapshot)=>{
+      let expenseItem = [];
+      QuerySnapshot.forEach((doc)=>{
+
+        expenseItem.push({id: doc.id, ...doc.data()});
+        setExpenseList([...expenseItem]);
+
+      })
+    })
+
+    
+  })
+
+
+
+  //navigate to add expense component
   const navigate = useNavigate();
-
   function goToAddExpense(){
-
     navigate("/addexpense")
-
   }
 
   return (
@@ -24,10 +47,28 @@ function Expenses() {
             <div className='mainContent'>
                 
                 <div className='headerTop'>
-                <h2>Expenses</h2>
-                <Button onClick={goToAddExpense}>Add Expenses</Button>
+                  <h2>Expenses</h2>
+                  <Button onClick={goToAddExpense}>Add Expenses</Button>
+                </div>
 
-              </div>
+                <div>
+
+
+                  {expenseList.map((myExpense)=>(
+                  <div  key={Math.random()}>
+                    <p>Amount: {myExpense.amount}</p>
+                    <p>Expense Date: {myExpense.expenseDate}</p>
+                    <p>Paid through: {myExpense.paidThrough}</p>
+                    <p>Authorised By: {myExpense.authorisedBy}</p>
+                    <hr/>
+                  </div>
+                  ))}
+                  
+
+
+
+
+                </div>
 
 
             </div>
--- src/Pages/Income.js
+++ src/Pages/Income.js
@@ -8,7 +8,7 @@ import {useNavigate} from "react-router-dom";
 import '../firebase';
 import {db} from '../firebase'
 import { collection, getDocs } from "firebase/firestore";
-import { async } from '@firebase/util';
+
 
 
 function Income() {
