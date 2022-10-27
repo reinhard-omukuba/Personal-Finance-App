4af32991f6fb8f00d7c895211c7c67653d601e38
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-10-27 09:39:15 +0300

    Pull all Income

--- src/Pages/Income.js
+++ src/Pages/Income.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LeftNav from '../Components/LeftNav'
 import NavBar from '../Components/NavBar'
 import { Button } from 'react-bootstrap'
@@ -7,10 +7,30 @@ import {useNavigate} from "react-router-dom";
 //firebase
 import '../firebase';
 import {db} from '../firebase'
+import { collection, getDocs } from "firebase/firestore";
+import { async } from '@firebase/util';
+
 
 function Income() {
 
+  const [incomeList, setIncomeList] = useState([]);
+
   //pull income data
+  useEffect(()=>{
+
+    const incomeData =  getDocs(collection(db, "income"));
+    incomeData.then((querySnapshot) => {
+      let incomeItem = [];
+      querySnapshot.forEach((doc) =>{
+        //console.log(doc.id, " => ", doc.data());
+        incomeItem.push({id: doc.id, ...doc.data()});
+        setIncomeList([...incomeItem]);
+      })
+  
+    })
+    
+  })
+
 
 
   //navigate to add income component
@@ -31,11 +51,25 @@ function Income() {
               <div className='headerTop'>
                 <h2>Income</h2>
                 <Button onClick={goToAddIncome}>Add Income</Button>
-
               </div>
+              <br/>
+
+              <div>
+
+                {incomeList.map((myincome) => (
+                  <div key={Math.random()}>
+                    <p>Amount: {myincome.amount}</p>
+                    <p>Income Account: {myincome.incomeAccount}</p>
+                    <p>Date: {myincome.incomeDate}</p>
+                    <p>Received By: {myincome.receivedBy}</p>
+                    <hr/>
+                  </div>
+                ))}
                 
 
 
+              </div>
+                
             </div>
 
         </div>
